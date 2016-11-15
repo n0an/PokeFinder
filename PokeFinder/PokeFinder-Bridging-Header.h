535d62ce8b574746ed6c60b7fe58ae5bcf23598f
n0an <nantog@gmail.com>
2016-11-15 21:09:59 +0300

    GeoFire Pod added

---
+++ PokeFinder/PokeFinder-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  PokeFinder-Bridging-Header.h
+//  PokeFinder
+//
+//  Created by Anton Novoselov on 15/11/2016.
+//  Copyright © 2016 Anton Novoselov. All rights reserved.
+//
+
+#import "GeoFire.h"
